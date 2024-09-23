1d69a3fe2acfd6f4f15fece285dd43c092a3464e
ImQasimShah <qsmkhan6@gmail.com>
2024-09-23 02:35:58 -0700

    first commit

---
+++ key.js
@@ -0,0 +1,14 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJbHBGsVjqaSj9v8Ht8GU_Rl06OquvCNw",
+    authDomain: "addpatient-a5fca.firebaseapp.com",
+    projectId: "addpatient-a5fca",
+    storageBucket: "addpatient-a5fca.appspot.com",
+    messagingSenderId: "283511674643",
+    appId: "1:283511674643:web:fd843a324dbafcc5cf6ce6"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
